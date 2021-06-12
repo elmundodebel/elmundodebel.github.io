49c84564d8c78a4bd7c1ef6b4be862e528ae3384
elmundodebel <83124283+elmundodebel@users.noreply.github.com>
2021-06-12 15:27:16 -0300

    Add files via upload

--- js/formGastos.js
+++ js/formGastos.js
@@ -1,37 +1,7 @@
-
-
-
-//ingresar gastos
 function FuncionIngresoGastos(){
-    $('#moduloGastos').append(FormGasto)
-    $('#IngresoGastos').attr("hidden",true)
+    $('#moduloGastos').append(FormGasto);
+    $('#IngresoGastos').attr("hidden",true);
     }
-//obtner datos del form intento fallido
-/*var gastos = [["gasto","detalle","rubro","fecha","monto","medio de pago"]]
-$('#IngresoGastos').click(function(event){
-    //event.preventDefault();
-    var gasto = gastos.length;
-    var detalle = $(this).find('input[id="detalleGasto"]').val();
-    var rubro = $(this).find('input[id="rubroGasto"]').val();
-    var fecha = $(this).find('input[id="fechaGasto"]').val();
-    var monto = $(this).find('input[id="montoGasto"]').val();
-    var mdp = $(this).find('input[id="mdpGasto"]').val();
-    gastos.push([gasto, detalle, rubro, fecha, monto, mdp])
-    localStorage.setItem('Gastos', JSON.stringify(gastos));   
-});
-*/
-//obtner datos del form
-
-function Cargar() {
-    var gasto = gastos.length;
-    var detalle = $('#formGastos').find('input[id="detalleGasto"]').val();
-    var rubro = $('#formGastos').find('input[id="rubroGasto"]').val();
-    var fecha = $('#formGastos').find('input[id="fechaGasto"]').val();
-    var monto = $('#formGastos').find('input[id="montoGasto"]').val();
-    var mdp = $('#formGastos').find('input[id="mdpGasto"]').val();
-    gastos.push([gasto, detalle, rubro, fecha, monto, mdp])
-    localStorage.setItem('Gastos', JSON.stringify(gastos));   
-}
 
 var FormGasto = `
 <div id = "gastosnuevo">
@@ -42,7 +12,7 @@ var FormGasto = `
                       <fieldset>
                         <div class="form-group">
                           <label for="detalleGasto">Detalle</label>
-                          <input class="form-control" id="detalleGasto" type="text" placeholder="Por ejemplo nafta" value ="">
+                          <input class="form-control" id="detalleGasto" type="text" placeholder="Por ejemplo nafta" value ="" >
                         </div>
                         <div class="form-group">
                           <label for="rubroGasto">Rubro</label>
@@ -50,11 +20,11 @@ var FormGasto = `
                         </div>
                         <div class="form-group">
                           <label for="fechaGasto">Fecha</label>
-                          <input class="form-control" id="fechaGasto" type="date">
+                          <input class="form-control" id="fechaGasto" type="date" >
                         </div>
                         <div class="form-group"> 
                           <label for="montoGasto">Monto en ARS</label>
-                          <input class="form-control" id="montoGasto" type="number">
+                          <input class="form-control" id="montoGasto" type="number" >
                         </div>
                         <div class="form-group"> 
                           <label for="mdpGasto">Medio de pago</label>
@@ -66,11 +36,9 @@ var FormGasto = `
                           <label for="ticketGasto">Ticket</label>
                           <input class="form-control" text= "ticket" id="ticketGasto" type="file" accept="image/*" multiple >
                         </div>
-
-                        
                         <button id = "prueba" class="btn btn-outline-primary" onclick= Cargar() >Cargar y Continuar </button>
                         <div></br></div>
-                        <button class="btn btn-outline-primary" type="submit">Cargar y Finalizar</button>
+                        <button class="btn btn-outline-primary" onclick= Cargar() >Cargar y Finalizar</button>
                         </div>
                       </fieldset>
                     </form>
@@ -78,3 +46,42 @@ var FormGasto = `
                 </div>
                   
               </div> `
+
+
+function Cargar() {
+    var gasto = gastos.length;
+    var detalle = $('#formGastos').find('input[id="detalleGasto"]').val();
+    var rubro = $('#formGastos').find('input[id="rubroGasto"]').val();
+    var fecha = $('#formGastos').find('input[id="fechaGasto"]').val();
+    var monto = $('#formGastos').find('input[id="montoGasto"]').val();
+    var mdp = $('#formGastos').find('input[id="mdpGasto"]').val();
+    gastos.push([gasto, detalle, rubro, fecha, monto, mdp])
+    localStorage.setItem('Gastos', JSON.stringify(gastos));
+       
+}
+
+
+// crea tabla con gastos
+function crearTabla(datosTabla) {
+  var tabla = document.createElement('table');
+  tabla.setAttribute("id","tablaDeGastos")
+  tabla.setAttribute("class","table table-hover")
+  var cuerpoTabla = document.createElement('tbody');
+
+  datosTabla.forEach(function(datosFilas) {
+    var fila = document.createElement('tr');
+
+    datosFilas.forEach(function(datosCeldas) {
+      var celda = document.createElement('td');
+      celda.appendChild(document.createTextNode(datosCeldas));
+      fila.appendChild(celda);
+    });
+
+    cuerpoTabla.appendChild(fila);
+  });
+
+  tabla.appendChild(cuerpoTabla);
+  document.getElementById("moduloGastos").appendChild(tabla);
+  $("#tablaDeGastos").hide()
+  $("#tablaDeGastos").show('slow')
+}
